01607b97da254e45b5bea457cb78285b360ba026
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-20 17:50:21 +0300

    commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import Link from "next/link";
+import Search from "./Search";
 
 export default function Navbar() {
   return (
@@ -7,6 +7,7 @@ export default function Navbar() {
       <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href="/">wikiRocket!</Link>
       </h1>
+      <Search />
     </nav>
   );
 }
--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -12,19 +12,21 @@ export default function Search() {
     setSearch("");
     router.push(`/${search}/`);
   };
-  <form
-    className="w-50 flex justify-center md:justtify-between"
-    onSubmit={handleSubmit}
-  >
-    <input
-      type="text"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      className="bg-white p-2 w-80 text-xl rounded-xl"
-      placeholder="Search"
-    />
-    <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
-      🚀
-    </button>
-  </form>;
+  return (
+    <form
+      className="w-50 flex justify-center md:justtify-between"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="bg-white p-2 w-80 text-xl rounded-xl"
+        placeholder="Search"
+      />
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+        🚀
+      </button>
+    </form>
+  );
 }
