cd94b3045ed40f34555d5ff5d5d1a7677ad5420c
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-20 11:17:30 +0300

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Home() {
-  return <div className=""></div>;
+  return <div className={inter.className}></div>;
 }
