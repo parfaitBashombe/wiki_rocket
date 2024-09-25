c3e0481dc817503b7918feb0d234d1d3450a882b
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-09-25 20:50:46 +0300

    modif

---
+++ app/[searchTerm]/components/item.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+type Props = {
+  result: Result;
+};
+
+export default function Item({ result }: Props) {
+  return <div>item</div>;
+}
--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -6,12 +6,39 @@ type Props = {
   };
 };
 
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
+  const data = await wikiData;
+  const displayTerm = searchTerm.replaceAll("%20", " ");
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`,
+    };
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search results for ${displayTerm}`,
+  };
+}
+
 export default async function SearchResults({ params: { searchTerm } }: Props) {
   const wikiData: Promise<SearchResult> = getWikiResults(searchTerm);
   const data = await wikiData;
   const results: Result[] | undefined = data?.query?.pages;
 
-  const content = <main className="bg-slate-200"></main>;
+  const content = (
+    <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
+      {results ? (
+        Object.values(results).map((result) => {
+          return <p>{JSON.stringify(result)}</p>;
+        })
+      ) : (
+        <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
+      )}
+    </main>
+  );
 
-  return <div>page</div>;
+  return content;
 }
